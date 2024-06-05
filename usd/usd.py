be777e0b6e6dc73c83a9b6e957018c46dd7488ee
Mohammad hosein Doostani <mohammadhoseindoostani@gmail.com>
2024-06-05 17:00:40 +0330

    Refactored Project

--- usd/usd.py
+++ usd/usd.py
@@ -1,98 +1,104 @@
 import csv
-import AssetRate
-
-
-def average(lst):
-    return round(sum(lst) / len(lst))
+from statistics import mean
+from common import asset_rate
 
 
 class USD:
-    def get_data():
-        def csv_dict_returner():
-            usd_date_list = []
+    def return_csv_list(self, file_name="USD_IRR.csv"):
+        csv_list = []
 
-            with open("USD_IRR.csv", encoding="utf-8-sig") as my_csv:
-                csv_dict = csv.DictReader(my_csv)
-                for row in csv_dict:
-                    dict_daily_model = {"date": "value", "price": "value"}
-                    dict_daily_model["date"] = row["Date"][:-3]
-                    dict_daily_model["price"] = round(float(row["Price"]))
-                    usd_date_list.append(dict_daily_model)
+        with open(file_name, encoding="utf-8-sig") as my_csv:
+            csv_dict = csv.DictReader(my_csv)
+            for row in csv_dict:
+                daily_model = {
+                    "date": row["Date"][:-3],
+                    "price": round(float(row["Price"])),
+                }
+                csv_list.append(daily_model)
 
-            return usd_date_list
+        return csv_list
 
-        def data_list_returner():
-            dict_monthly_model = {"month": "value", "data": "value"}
-            month_list = ["1360/7"]
-            data_list = []
-            rate_list = []
+    def return_daily_prices(self, csv_list):
+        monthly_model = {"month": "value", "data": "value"}
+        month_list = ["1360/7"]
+        daily_prices = []
+        rate_list = []
 
-            for i in csv_dict_returner():
-                if i["date"] in month_list:
-                    rate_list.append(i["price"])
-                else:
-                    if rate_list:
-                        dict_monthly_model["month"] = month_list[-1]
-                        dict_monthly_model["data"] = rate_list
-                        data_list.append(dict_monthly_model)
-                        rate_list = []
+        for day in csv_list:
+            if day["date"] in month_list:
+                rate_list.append(day["price"])
+            else:
+                if rate_list:
+                    monthly_model["month"] = month_list[-1]
+                    monthly_model["data"] = rate_list
+                    daily_prices.append(monthly_model)
+                    rate_list = []
 
-                    month_list.append(i["date"])
-                    rate_list.append(i["price"])
-                    dict_monthly_model = {"month": i["date"], "data": rate_list}
+                month_list.append(day["date"])
+                rate_list.append(day["price"])
+                monthly_model = {"month": day["date"], "data": rate_list}
 
-            if rate_list:
-                dict_monthly_model["data"] = rate_list
-                data_list.append(dict_monthly_model)
+        if rate_list:
+            monthly_model["data"] = rate_list
+            daily_prices.append(monthly_model)
 
-            return data_list
+        return daily_prices
 
-        def main_list_returner():
-            dict_monthly_model = {
+    def return_monthly_prices(self, daily_prices):
+        monthly_model = {
+            "year": "value",
+            "month": "value",
+            "average": "value",
+            "change": "value",
+        }
+        monthly_prices = []
+        previous_price = round(
+            mean(daily_prices[0]["data"])
+        )  # quantification because of the first block of database
+
+        for month in daily_prices:
+            monthly_model["year"] = month["month"][:4]
+            monthly_model["month"] = month["month"][5:]
+            monthly_model["average"] = round(mean(month["data"]))
+            monthly_model["change"] = round(
+                (monthly_model["average"] - previous_price) / previous_price, 2
+            )
+
+            if monthly_model["change"] == 0:
+                monthly_model["change"] = 0
+            # avoiding -0
+
+            monthly_prices.append(monthly_model)
+
+            previous_price = monthly_model["average"]
+            monthly_model = {
                 "year": "value",
                 "month": "value",
                 "average": "value",
                 "change": "value",
             }
-            main_list = []
-            previous_price = average(
-                data_list_returner()[0]["data"]
-            )  # quantification because of the first block of database
 
-            for j in data_list_returner():
-                dict_monthly_model["year"] = j["month"][:4]
-                dict_monthly_model["month"] = j["month"][5:]
-                dict_monthly_model["average"] = average(j["data"])
-                dict_monthly_model["change"] = round(
-                    (dict_monthly_model["average"] - previous_price) / previous_price, 2
+        return monthly_prices
+
+    def write_asset_rate(self, monthly_prices):
+        asset_rate_list = []
+
+        for month_data in monthly_prices:
+            asset_rate_list.append(
+                asset_rate.AssetRate(
+                    month_data["year"],
+                    month_data["month"],
+                    month_data["average"],
+                    month_data["change"],
                 )
+            )
 
-                if dict_monthly_model["change"] == 0:
-                    dict_monthly_model["change"] = 0
-                # avoiding -0
+        return asset_rate_list
 
-                main_list.append(dict_monthly_model)
+    def get_data(self):
+        csv_list = self.return_csv_list()
+        daily_prices = self.return_daily_prices(csv_list)
+        monthly_prices = self.return_monthly_prices(daily_prices)
+        asset_rate_list = self.write_asset_rate(monthly_prices)
 
-                previous_price = dict_monthly_model["average"]
-                dict_monthly_model = {
-                    "year": "value",
-                    "month": "value",
-                    "average": "value",
-                    "change": "value",
-                }
-
-            return main_list
-
-        def asset_rate_writing():
-            asset_rate_list = []
-
-            for k in main_list_returner():
-                asset_rate_list.append(
-                    AssetRate.AssetRate(
-                        k["year"], k["month"], k["average"], k["change"]
-                    )
-                )
-
-            return asset_rate_list
-
-        return asset_rate_writing()
+        return asset_rate_list
